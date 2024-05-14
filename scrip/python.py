75d22ccb3590b4c9c1253b7674f3d7d7fde3f4a6
momo <maureenmutoni1@gmail.com>
2024-05-13 19:28:20 -0700

    new

---
+++ scrip/python.py
@@ -0,0 +1,6 @@
+import os
+
+print ("hello Maureen")
+
+os.system("nproc")
+os.system("uname -r")
---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "ENVIRONMENT" {
+  default = main
+}
+variable"REGION"{
+default = us-east-1
+}
+
+variable "INSTANCE_TYPE" {
+  default = t2.micro
+}
